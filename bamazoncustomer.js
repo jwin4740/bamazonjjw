ee53f99b7c766c01f904176ffdb5640ad987a1dd
James <jwin4740@gmail.com>
2017-02-28 14:58:34 -0500

    made username capable

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -1,4 +1,6 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
+var moment = require("moment");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -8,13 +10,96 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id" + connection.threadId);
+// connection.connect(function(err) {
+//     if (err) throw err;
+//     console.log("connected as id" + connection.threadId);
+// });
+
+// // select entire table
+// connection.query("SELECT * FROM products", function (err, res){
+// 	if(err) throw err;
+// 	console.log(res);
+// });
+
+var currentUser;
+var password;
+inquirer.prompt({
+    name: "usertype",
+    type: "list",
+    message: "Are you a new user or returning user",
+    choices: ["newuser", "returninguser"]
+}).then(function(answer) {
+    if (answer.usertype === "newuser") {
+        createNewUser();
+    } else {
+        inquirer.prompt([{
+            name: "user",
+            type: "input",
+            message: "USERNAME:"
+        }, {
+            name: "pass",
+            type: "password",
+            message: "PASSWORD (case sensitive):"
+        }]).then(function(answer) {
+            currentUser = answer.user;
+            password = answer.pass;
+            console.log(currentUser + "\n" + password);
+            connection.query("INSERT INTO useraccounts SET ?", {
+                username: currentUser,
+                password: password,
+                last_login: moment()
+            }, function(err, res) {
+                if (err) throw err;
+                console.log(res);
+            });
+
+
+
+        })
+    }
+
+
 });
 
-// select entire table
-connection.query("SELECT * FROM products", function (err, res){
-	if(err) throw err;
-	console.log(res);
-});
+function createNewUser() {
+    inquirer.prompt([{
+        name: "user",
+        type: "input",
+        message: "USERNAME:"
+    }, {
+        name: "pass",
+        type: "password",
+        message: "PASSWORD (case sensitive):"
+    }]).then(function(answer) {
+        currentUser = answer.user;
+        password = answer.pass;
+        console.log(currentUser + "\n" + password);
+        connection.query("INSERT INTO useraccounts SET ?", {
+            username: currentUser,
+            password: password,
+            last_login: moment()
+        }, function(err, res) {
+            if (err) throw err;
+            console.log(res);
+        });
+
+        connection.query("CREATE TABLE " + currentUser + " (id INTEGER(10) NOT NULL AUTO_INCREMENT PRIMARY KEY, account_balance DECIMAL(8, 2), purchase VARCHAR(100), purchase_date VARCHAR(100), cost DECIMAL(8, 2));",
+            function(err, res) {
+                if (err) throw err;
+                console.log(res);
+            });
+
+        connection.query("INSERT INTO " + currentUser + " SET ?", {
+            account_balance: 1000,
+            purchase: "iphone",
+            purchase_date: "today",
+            cost: 250
+        }, function(err, res) {
+            if (err) throw err;
+            console.log(res);
+        });
+
+
+
+    })
+}
