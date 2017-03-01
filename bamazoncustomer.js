40125e7fb3123746e416ed57bc69e9d8cb0fe9fa
James <jwin4740@gmail.com>
2017-02-28 23:15:52 -0500

    added new database

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "platinum",
-    database: "bamazon"
+
 });
 
 // connection.connect(function(err) {
@@ -55,15 +55,19 @@ var start = function() {
 start();
 
 function verifyReturningUser() {
-    connection.query("SELECT username, password FROM useraccounts WHERE username='" + currentUser + "' AND password='" + password + "';", function(err, res) {
+    connection.query("SELECT username, password FROM bamazon.useraccounts WHERE username='" + currentUser + "' AND password='" + password + "';", function(err, res) {
         if (err) throw err;
         if (res == "") {
             console.log("\nINVALID USERNAME/PASSWORD COMBINATION\n");
             setTimeout(start, 1500);
         } else {
             console.log("\nverifying...");
-            setTimeout(function() { console.log("\n WELCOME TO BAMAZON " + currentUser) }, 2000);
+            setTimeout(function() { console.log("\n\n\n\n WELCOME TO BAMAZON " + currentUser) }, 2000);
         }
+        connection.query("UPDATE bamazon.useraccounts SET last_login='" + moment().format('llll') + "' WHERE username='" + currentUser + "';", function(err, res) {
+            if (err) throw err;
+
+        });
     });
 
 }
@@ -77,36 +81,48 @@ function createNewUser() {
         name: "pass",
         type: "password",
         message: "PASSWORD (case sensitive):"
+    }, {
+        name: "passconfirm",
+        type: "password",
+        message: "CONFIRM PASSWORD:"
     }]).then(function(answer) {
-        currentUser = answer.user;
-        password = answer.pass;
-        console.log(currentUser + "\n" + password);
-        connection.query("INSERT INTO useraccounts SET ?", {
-            username: currentUser,
-            password: password,
-            last_login: moment()
-        }, function(err, res) {
-            if (err) throw err;
-            console.log(res);
-        });
 
-        connection.query("CREATE TABLE " + currentUser + " (id INTEGER(10) NOT NULL AUTO_INCREMENT PRIMARY KEY, account_balance DECIMAL(8, 2), purchase VARCHAR(100), purchase_date VARCHAR(100), cost DECIMAL(8, 2));",
-            function(err, res) {
-                if (err) throw err;
-                console.log(res);
-            });
-
-        connection.query("INSERT INTO " + currentUser + " SET ?", {
-            account_balance: 1000,
-            purchase: "iphone",
-            purchase_date: "today",
-            cost: 250
-        }, function(err, res) {
-            if (err) throw err;
-            console.log(res);
-        });
-
-
-
+        if (answer.pass === answer.passconfirm) {
+            currentUser = answer.user;
+            password = answer.pass;
+            newUserConfirmed();
+        } else {
+            console.log("\nPASSWORDS DO NOT MATCH, TRY AGAIN\n");
+            setTimeout(createNewUser, 1000);
+        }
     })
+}
+
+function newUserConfirmed() {
+	setTimeout(function() { console.log("\nYOUR ACCOUNT HAS BEEN SUCCESSFULLY CREATED\n\n\n\n WELCOME TO BAMAZON " + currentUser); }, 1000);
+
+    connection.query("INSERT INTO bamazon.useraccounts SET ?", {
+        username: currentUser,
+        password: password,
+        account_created: moment(),
+        last_login: moment().format('llll')
+    }, function(err, res) {
+        if (err) throw err;
+
+    });
+
+    connection.query("CREATE TABLE bamazon_user_management." + currentUser + " (id INTEGER(10) NOT NULL AUTO_INCREMENT PRIMARY KEY, account_balance DECIMAL(8, 2), purchase VARCHAR(100), purchase_date VARCHAR(100), cost DECIMAL(8, 2));",
+        function(err, res) {
+            if (err) throw err;
+
+        });
+
+    connection.query("INSERT INTO bamazon_user_management." + currentUser + " SET ?", {
+        account_balance: 1000,
+        purchase: "iphone",
+        purchase_date: "today",
+        cost: 250
+    }, function(err, res) {
+        if (err) throw err;
+    });
 }
